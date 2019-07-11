4ba0d389709788a7342bd14de9198e17dd055080
Trevor Baron <trevorjbaron@gmail.com>
2019-07-11 00:19:24 -0700

    thre

--- testApp/app.ts
+++ testApp/app.ts
@@ -1,9 +1,74 @@
 declare var io: any;
+import * as THREE from "three";
 
+var vr = require("./vr")
+//import * as vr from "./vr"
 
 var uuid = createUUID();
 var socket: SocketIO.Socket = io();
 var isHub = window.location.search.length > 2;
+var remoteVideo = document.createElement('video');
+remoteVideo.autoplay = true
+
+var width = window.innerWidth;
+var height = window.innerHeight;
+var viewAngle = 45;
+var nearClipping = 0.1;
+var farClipping = 9999;
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( viewAngle, width / height, nearClipping, farClipping );
+var tmp = console.log;
+//console.log = ()=>{}
+var renderer = new THREE.WebGLRenderer();
+renderer.vr.enabled = true
+var c= (renderer.vr as any).getController(0)
+console.log(c)
+document.body.appendChild(vr.default.createButton(renderer, undefined));
+//console.log(renderer)
+//console.log = tmp
+renderer.setSize( width, height );
+document.body.appendChild( renderer.domElement );
+
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+}
+animate();
+
+var cubeGeometry = new THREE.PlaneGeometry( 1920/1080, 1 );
+var t = new THREE.VideoTexture(remoteVideo)
+var cubeMaterial = new THREE.MeshLambertMaterial( { map: t } );
+var cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+scene.add(cube);
+
+cube.position.x = -2
+cube.position.z = -5;
+
+var light = new THREE.PointLight(0xFFFFFF);
+light.position.x = 0;
+light.position.y = 10;
+light.position.z = 0;
+scene.add(light);
+
+document.onmousedown = (e)=>{
+    var mouse = new THREE.Vector2();
+    var raycaster = new THREE.Raycaster();
+    mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+    
+    // update the picking ray with the camera and mouse position
+	raycaster.setFromCamera( mouse, camera );
+
+	// calculate objects intersecting the picking ray
+    var intersects = raycaster.intersectObjects( [cube] );
+    if(intersects[0] && intersects[0].uv){
+        console.log(intersects[0].uv)
+        socket.emit("robot", {action: "mouseMove", x: intersects[0].uv.x, y: 1-intersects[0].uv.y})
+    }
+    
+}
+
+
 
 var main = async () => {
     // Create video element
@@ -13,11 +78,7 @@ var main = async () => {
     localVideo.autoplay = true
     document.body.appendChild(localVideo)
 
-    var remoteVideo = document.createElement('video');
-    // remoteVideo.width = 100;
-    // remoteVideo.height = 100;
-    remoteVideo.autoplay = true
-    document.body.appendChild(remoteVideo)
+    
 
     // Create desktop stream
     var media: null | MediaStream = null;
