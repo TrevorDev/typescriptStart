44642edc5c86766540d08d4c7117b0a347be9f02
Trevor Baron <trevorjbaron@gmail.com>
2019-08-04 09:16:56 -0700

    update config

--- server.ts
+++ server.ts
@@ -1,8 +1,15 @@
 import * as express from "express"
 import * as http from "http"
 import * as sio from "socket.io";
+import * as program from "commander"
 
-var port = 3000;
+program
+  .version('0.1.0')
+  .option('-p, --port [port]', 'port to run on')
+  .parse(process.argv)
+
+var port = program.port ? program.port : 3000
+console.log("Starting on port: "+port)
 
 // Basic express webserver
 var app = express()
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+// https://webpack.js.org/guides/typescript/
+
+const NodemonPlugin = require('nodemon-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+
+module.exports = [
+  {
+    mode: 'development',
+    entry: './server.ts',
+    devtool: "inline-source-map",
+    target: "node",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    externals: [nodeExternals()],
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+      filename: 'server.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+      new NodemonPlugin({
+        script: './dist/server.js'
+      })
+    ]
+  },
+  {
+    mode: 'development',
+    entry: "./testApp/app.ts",
+    devtool: "inline-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+      filename: 'testApp.js',
+      path: path.resolve(__dirname, 'public/dist')
+    }
+  }
+]
